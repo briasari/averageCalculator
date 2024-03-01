68d081e3304dffd8f0ad0972a0b70a66a4f41141
briasari <briasari660@gmail.com>
2024-03-01 14:32:14 -0500

    Add project files.

---
+++ averageCalculator/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace averageCalculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.infoLabel = new System.Windows.Forms.Label();
+            this.infoLabel2 = new System.Windows.Forms.Label();
+            this.outputLabel = new System.Windows.Forms.Label();
+            this.resetButton = new System.Windows.Forms.Button();
+            this.addButton = new System.Windows.Forms.Button();
+            this.titleLabel = new System.Windows.Forms.Label();
+            this.numInput = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // infoLabel
+            // 
+            this.infoLabel.BackColor = System.Drawing.Color.Transparent;
+            this.infoLabel.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.infoLabel.ForeColor = System.Drawing.Color.White;
+            this.infoLabel.Location = new System.Drawing.Point(52, 87);
+            this.infoLabel.Name = "infoLabel";
+            this.infoLabel.Size = new System.Drawing.Size(323, 23);
+            this.infoLabel.TabIndex = 2;
+            this.infoLabel.Text = "Enter a number and press add";
+            this.infoLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // infoLabel2
+            // 
+            this.infoLabel2.BackColor = System.Drawing.Color.Transparent;
+            this.infoLabel2.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.infoLabel2.ForeColor = System.Drawing.Color.White;
+            this.infoLabel2.Location = new System.Drawing.Point(-6, 115);
+            this.infoLabel2.Name = "infoLabel2";
+            this.infoLabel2.Size = new System.Drawing.Size(439, 23);
+            this.infoLabel2.TabIndex = 3;
+            this.infoLabel2.Text = "(enter 0 and press Add to calculate average)";
+            this.infoLabel2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.outputLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.outputLabel.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.outputLabel.ForeColor = System.Drawing.Color.Black;
+            this.outputLabel.Location = new System.Drawing.Point(65, 279);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(298, 77);
+            this.outputLabel.TabIndex = 4;
+            this.outputLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // resetButton
+            // 
+            this.resetButton.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.resetButton.Location = new System.Drawing.Point(342, 409);
+            this.resetButton.Name = "resetButton";
+            this.resetButton.Size = new System.Drawing.Size(75, 29);
+            this.resetButton.TabIndex = 5;
+            this.resetButton.Text = "Reset";
+            this.resetButton.UseVisualStyleBackColor = true;
+            this.resetButton.Click += new System.EventHandler(this.resetButton_Click);
+            // 
+            // addButton
+            // 
+            this.addButton.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.addButton.Location = new System.Drawing.Point(249, 211);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(75, 29);
+            this.addButton.TabIndex = 0;
+            this.addButton.Text = "Add";
+            this.addButton.UseVisualStyleBackColor = true;
+            this.addButton.Click += new System.EventHandler(this.addButton_Click);
+            // 
+            // titleLabel
+            // 
+            this.titleLabel.BackColor = System.Drawing.Color.Maroon;
+            this.titleLabel.Font = new System.Drawing.Font("Microsoft YaHei", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titleLabel.ForeColor = System.Drawing.Color.White;
+            this.titleLabel.Location = new System.Drawing.Point(-3, 0);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(436, 66);
+            this.titleLabel.TabIndex = 1;
+            this.titleLabel.Text = "Average Number Solver";
+            this.titleLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // numInput
+            // 
+            this.numInput.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numInput.Location = new System.Drawing.Point(96, 211);
+            this.numInput.Name = "numInput";
+            this.numInput.Size = new System.Drawing.Size(138, 29);
+            this.numInput.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(429, 450);
+            this.Controls.Add(this.numInput);
+            this.Controls.Add(this.resetButton);
+            this.Controls.Add(this.outputLabel);
+            this.Controls.Add(this.infoLabel2);
+            this.Controls.Add(this.infoLabel);
+            this.Controls.Add(this.titleLabel);
+            this.Controls.Add(this.addButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label infoLabel;
+        private System.Windows.Forms.Label infoLabel2;
+        private System.Windows.Forms.Label outputLabel;
+        private System.Windows.Forms.Button resetButton;
+        private System.Windows.Forms.Button addButton;
+        private System.Windows.Forms.Label titleLabel;
+        private System.Windows.Forms.TextBox numInput;
+    }
+}
+
---
+++ averageCalculator/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace averageCalculator
+{
+    public partial class Form1 : Form
+    {
+
+        double average;
+        double num;
+        double sum;
+        int count;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void addButton_Click(object sender, EventArgs e)
+        {
+
+            num = Convert.ToInt32(numInput.Text);
+            {
+                if (num != 0)
+                {
+                    count++;
+                    sum += num;
+                    outputLabel.Text = $"{num} was added to the sum";
+                }
+                else
+                {
+                    average = (sum / count);
+                    outputLabel.Text = $"{average} is your average";
+                }
+
+            }
+        }
+
+            private void resetButton_Click(object sender, EventArgs e)
+            {
+                average = 0;
+                num = 0;
+                sum = 0;
+                count = 0;
+
+                outputLabel.Text = "";
+            }
+        }
+    }
